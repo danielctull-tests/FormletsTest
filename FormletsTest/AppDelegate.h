78cee93c9ff3e4441b678dc5091dac911915dfee
Daniel Tull <dt@danieltull.co.uk>
2013-04-02 17:10:00 +0100

    Initial Commit

---
+++ FormletsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FormletsTest
+//
+//  Created by Daniel Tull on 02.04.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
